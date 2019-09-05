870b8fb0ce0d61555a6b48f21a1f8ee6de3f2a1e
Ryanho <ryanho2013@163.com>
2019-09-05 13:54:19 +0800

    add Python Solutions

---
+++ Python/01TwoSum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        hashmap = {}
+        for index, num in enumerate(nums):
+            another_num = target - num
+            if another_num in hashmap:
+                return [hashmap[another_num], index]
+            hashmap[num] = index
+        return None
+
+s1 = Solution()
+nums = [2,7,11,15]
+target = 9
+print(s1.twoSum(nums,target))
